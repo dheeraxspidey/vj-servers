24fd695a2102af7d04b27aecfe5d84fa128e5d4a
KP <head.iie@vnrvjiet.in>
2025-02-20 10:10:55 +0530

    Modify the port number for Navi App #18

--- frontend/fe7-navi/app.py
+++ frontend/fe7-navi/app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=3117, debug=True)
+    app.run(host="0.0.0.0",port=3107, debug=True)
