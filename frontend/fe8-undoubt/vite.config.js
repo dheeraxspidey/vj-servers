4faa2791302e4633f43650a76ef4cc7315794854
KP <head.iie@vnrvjiet.in>
2025-02-20 07:09:48 +0530

    Undoubt: minor fixes #19

--- frontend/fe8-undoubt/vite.config.js
+++ frontend/fe8-undoubt/vite.config.js
@@ -15,7 +15,13 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0', // Allow connections from the local network
-    port: 3108, // Specify the port (default is 3108)
-  },
+        host: '0.0.0.0', // Allow connections from the local network
+        port: 3108, // Specify the port (default is 3108)
+        allowedHosts: [
+          '127.0.0.1', // or 'localhost' - Important for dev server access
+          'undoubt.vnrzone.site', // Add your domains here
+          'campus.vnrzone.site', 
+          '10.45.8.186' 
+        ]
+      }
 })
