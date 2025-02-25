de10a0df0938473f872daef6c496810c0f72b97e
KP <head.iie@vnrvjiet.in>
2025-02-25 09:05:31 +0530

    Give correct port number to access externally. #23

--- frontend/super-app/vite.config.js
+++ frontend/super-app/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Super App Config
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host: '0.0.0.0',
+    port:3102,
+    allowedHosts: [
+      '127.0.0.1', // or 'localhost' - Important for dev server access
+      '103.248.208.119' 
+    ],
+  }
 })
